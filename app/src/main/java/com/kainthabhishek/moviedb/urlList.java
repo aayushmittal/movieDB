d2330899bcfec85cb794fab53dd5dbc2f519c827
Aayush <aayushmittal1996@gmail.com>
2016-03-27 13:35:44 +0530

    final

--- app/src/main/java/com/kainthabhishek/moviedb/urlList.java
+++ app/src/main/java/com/kainthabhishek/moviedb/urlList.java
@@ -4,8 +4,7 @@ package com.kainthabhishek.moviedb;
  * Created by aayush on 27/3/16.
  */
 public class urlList {
-    public static String toprated = "toprated";
-    public static String nowPlaying = "playing";
-    public static String popular = "populae";
-
+    public static String toprated = "http://api.themoviedb.org/3/movie/top_rated";
+    public static String nowPlaying = "http://api.themoviedb.org/3/movie/popular";
+    public static String popular = "http://api.themoviedb.org/3/movie/now_playing";
 }
